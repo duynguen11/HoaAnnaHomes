6dc33cd7eeb369f2e67ffeda690c62e6dd71ad72
duynguen11 <duynguen11@gmail.com>
2024-12-15 17:37:03 +0700

    Update Homepage 05:36 PM

--- src/components/IntroSection/IntroSection.jsx
+++ src/components/IntroSection/IntroSection.jsx
@@ -1,4 +1,3 @@
-
 const IntroSection = () => {
   return (
     <div className="p-6">
@@ -7,7 +6,10 @@ const IntroSection = () => {
           <h2 className="w-full mb-6 text-3xl font-bold text-blue-900">
             Hoà Anna Homes
           </h2>
-          <p className="text-sm mb-6 text-gray-600">Sỡ hữu nhà dễ dàng hơn <br/>An toàn hơn hạnh phúc hơn.</p>
+          <p className="text-sm mb-6 text-gray-600">
+            Sỡ hữu nhà dễ dàng hơn <br />
+            An toàn hơn hạnh phúc hơn.
+          </p>
         </div>
         <div>
           <p className="flex justify-evenly text-sm mb-6 text-gray-600">
@@ -23,8 +25,7 @@ const IntroSection = () => {
             Không chỉ là một Căn Nhà, mà còn là một Tổ Ấm.
           </p>
           <p className="text-sm text-gray-600">
-            Sự hài lòng của Khách hàng - là điều Hoà Anna Homes trân quý
-            nhất.
+            Sự hài lòng của Khách hàng - là điều Hoà Anna Homes trân quý nhất.
           </p>
         </div>
       </div>
@@ -33,39 +34,55 @@ const IntroSection = () => {
       </div>
       <div className="max-w-7xl mx-auto text-center">
         <h2 className="pt-6 text-3xl font-bold text-blue-900 mb-6">
-          Hoạt động chính của Hoà Anna Homes
+          Giới thiệu Dịch Vụ Bất Động Sản Hoà Anna Homes
         </h2>
         <p className="text-sm text-gray-600 mb-12">
-          Tư Vấn - Quản Lý - Tuyển Dụng
+          Tại HoaAnna & Homes, chúng tôi mang đến các dịch vụ bất động sản
+          chuyên nghiệp, đáp ứng mọi nhu cầu của khách hàng:
         </p>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           <div className="bg-blue-900 text-white px-6 py-8 rounded shadow-lg">
-            <h3 className="text-xl font-semibold mb-4">Dịch Vụ Về BĐS</h3>
+            <h3 className="text-xl font-semibold mb-4">
+              Dịch Vụ Mua Bán Và Chuyển Nhượng
+            </h3>
             <p className="text-sm">
-              Tư vấn mua mới, chuyển nhượng, cho thuê và quản lý khai thác tài
-              sản
+              • Tư vấn mua bán, chuyển nhượng bất động sản từ thị trường sơ cấp
+              đến thứ cấp. <br />• Tập trung vào các dự án cao cấp như căn hộ,
+              shophouse thương mại và nhà phố, phù hợp với nhu cầu nhà ở và đầu
+              tư.
             </p>
           </div>
           <div className="bg-blue-900 text-white px-6 py-8 rounded shadow-lg">
-            <h3 className="text-xl font-semibold mb-4">Tư Vấn Tài Chính</h3>
+            <h3 className="text-xl font-semibold mb-4">
+              {" "}
+              Dịch Vụ Cho Thuê Và Quản Lý Tài Sản
+            </h3>
             <p className="text-sm">
-              Giải pháp tài chính giúp khách hàng mua căn nhà phù hợp với ngân
-              sách
+              • Hỗ trợ khách hàng tìm kiếm và cho thuê căn hộ, nhà ở, văn phòng
+              một cách hiệu quả và nhanh chóng. <br />• Dịch vụ quản lý tài sản
+              chuyên nghiệp, giúp khai thác tối ưu giá trị bất động sản.
             </p>
           </div>
           <div className="bg-blue-900 text-white px-6 py-8 rounded shadow-lg">
-            <h3 className="text-xl font-semibold mb-4">Xây Dựng Đội Ngũ</h3>
+            <h3 className="text-xl font-semibold mb-4">
+              Tư Vấn Giải Pháp Tài Chính
+            </h3>
             <p className="text-sm">
-              Không ngừng tuyển dụng mở rộng đội ngũ lan tỏa dịch vụ BĐS tốt
-              nhất
+              • Hỗ trợ khách hàng lựa chọn các giải pháp tài chính phù hợp, giúp
+              việc sở hữu bất động sản trở nên dễ dàng và thuận lợi hơn. <br />•
+              Đồng hành trong việc hoạch định ngân sách đầu tư và mua bán bất
+              động sản.
             </p>
           </div>
           <div className="bg-blue-900 text-white px-6 py-8 rounded shadow-lg">
-            <h3 className="text-xl font-semibold mb-4">Đào Tạo Chuyên Môn</h3>
+            <h3 className="text-xl font-semibold mb-4">
+              Xây Dựng Mối Quan Hệ Và Đội Ngũ Chuyên Nghiệp
+            </h3>
             <p className="text-sm">
-              Bộ giáo trình đào tạo từ nghiên cứu chuyên sâu và kinh nghiệm thực
-              tế
+              • Tập trung xây dựng mạng lưới đối tác rộng khắp, mang lại giá trị
+              tối ưu cho khách hàng. <br />• Cam kết mang đến dịch vụ tận tâm,
+              chuyên nghiệp và đáng tin cậy.
             </p>
           </div>
         </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,7 @@
 import Header from "../components/Header/Header";
 import SlideSection from "../components/SlideSection/SlideSection";
 import IntroSection from "../components/IntroSection/IntroSection";
-import AchievementsSection from "../components/AchievementsSection/AchievementsSection";
 import FeedbackSection from "../components/FeedbackSection/FeedbackSection";
-import MarketsSection from "../components/MarketsSection/MarketsSection";
 import FloatingContact from "../components/FloatingContact/FloatingContact";
 import Footer from "../components/Footer/Footer";
 
@@ -13,9 +11,7 @@ function HomePage() {
       <Header />
       <SlideSection />
       <IntroSection />
-      <AchievementsSection />
       <FeedbackSection />
-      <MarketsSection />
       <FloatingContact />
       <Footer />
     </>
