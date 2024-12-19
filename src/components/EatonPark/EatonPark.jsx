7f6cf131715ee40063b2574e15fa53596af5a3f1
duynguen11 <duynguen11@gmail.com>
2024-12-19 13:13:00 +0700

    EatonPark upload

--- src/components/EatonPark/EatonPark.jsx
+++ src/components/EatonPark/EatonPark.jsx
@@ -6,7 +6,23 @@ import EatonPark3 from "../../assets/images/EatonPark/EatonPark3.jpg";
 import EatonPark4 from "../../assets/images/EatonPark/EatonPark4.jpg";
 import EatonPark5 from "../../assets/images/EatonPark/EatonPark5.jpg";
 import EatonPark6 from "../../assets/images/EatonPark/EatonPark6.jpg";
+import EatonPark7 from "../../assets/images/EatonPark/EatonPark7.jpg";
+import EatonPark8 from "../../assets/images/EatonPark/EatonPark8.jpg";
+import EatonPark9 from "../../assets/images/EatonPark/EatonPark9.jpg";
 
+import Vitri1 from "../../assets/images/EatonPark/Vitri1.jpg";
+import Vitri2 from "../../assets/images/EatonPark/Vitri2.jpg";
+import Vitri3 from "../../assets/images/EatonPark/Vitri3.jpg";
+import Vitri4 from "../../assets/images/EatonPark/Vitri4.jpg";
+import Vitri5 from "../../assets/images/EatonPark/Vitri5.jpg";
+import Vitri6 from "../../assets/images/EatonPark/Vitri6.jpg";
+import Vitri7 from "../../assets/images/EatonPark/Vitri7.jpg";
+import Vitri8 from "../../assets/images/EatonPark/Vitri8.jpg";
+import Vitri9 from "../../assets/images/EatonPark/Vitri9.jpg";
+import Vitri10 from "../../assets/images/EatonPark/Vitri10.jpg";
+import Vitri11 from "../../assets/images/EatonPark/Vitri11.jpg";
+
+import { CheckIcon } from "@heroicons/react/20/solid";
 import FloatContact from "../../components/FloatingContact/FloatingContact";
 
 const EatonPark = () => {
@@ -41,10 +57,96 @@ const EatonPark = () => {
       content: "Slide 6",
       image: EatonPark6,
     },
+    {
+      id: 7,
+      content: "Slide 7",
+      image: EatonPark7,
+    },
+    {
+      id: 8,
+      content: "Slide 8",
+      image: EatonPark8,
+    },
+    {
+      id: 9,
+      content: "Slide 9",
+      image: EatonPark9,
+    },
+  ];
+  const areas = [
+    {
+      id: 1,
+      content: "Slide 1",
+      image: Vitri1,
+    },
+    {
+      id: 2,
+      content: "Slide 2",
+      image: Vitri2,
+    },
+    {
+      id: 3,
+      content: "Slide 3",
+      image: Vitri3,
+    },
+    {
+      id: 4,
+      content: "Slide 4",
+      image: Vitri4,
+    },
+    {
+      id: 5,
+      content: "Slide 5",
+      image: Vitri5,
+    },
+    {
+      id: 6,
+      content: "Slide 6",
+      image: Vitri6,
+    },
+    {
+      id: 7,
+      content: "Slide 7",
+      image: Vitri7,
+    },
+    {
+      id: 8,
+      content: "Slide 8",
+      image: Vitri8,
+    },
+    {
+      id: 9,
+      content: "Slide 9",
+      image: Vitri9,
+    },
+    {
+      id: 10,
+      content: "Slide 10",
+      image: Vitri10,
+    },
+    {
+      id: 11,
+      content: "Slide 11",
+      image: Vitri11,
+    },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isPaused, setIsPaused] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false); // Điều khiển mở modal
+  const [modalImage, setModalImage] = useState(null);
+
+  // Mở modal và hiển thị ảnh
+  const openModal = (image) => {
+    setModalImage(image); // Lưu ảnh được click
+    setIsModalOpen(true); // Mở modal
+  };
+
+  // Đóng modal
+  const closeModal = () => {
+    setIsModalOpen(false); // Đóng modal
+    setModalImage(null); // Xóa ảnh trong modal
+  };
 
   useEffect(() => {
     if (!isPaused) {
@@ -60,22 +162,20 @@ const EatonPark = () => {
   return (
     <div className="p-6">
       <div className="max-w-7xl mx-auto">
-        <h2 className="text-2xl font-bold text-blue-900 mb-6">
+        <h2 className="text-xl font-bold text-blue-900 my-6">
           Thông tin dự án Eaton Park ở 94A Mai Chí Thọ, Phường An Phú, Thành phố
           Thủ Đức, Thành phố Hồ Chí Minh cập nhật mới nhất tháng 12/2024 từ Hòa
           Anna Homes
         </h2>
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-[2fr_1fr] lg:grid-cols-[2fr_1fr]">
-          <div className="bg-gray-50 rounded p-6">
-            <h2 className="text-2xl font-bold text-blue-900 mb-2">
-              EATON PARK
-            </h2>
+          <div className="rounded py-6">
+            <h2 className="text-xl font-bold text-blue-900 mb-2">EATON PARK</h2>
             <p className="text-sm text-gray-600 mb-3">
               94A Mai Chí Thọ, Phường An Phú, Thành phố Thủ Đức, Thành phố Hồ
               Chí Minh
             </p>
             <div
-              className="w-full mx-auto rounded relative overflow-hidden h-[35vh] md:h-[50vh] lg:h-[55vh]"
+              className="w-full mx-auto rounded relative overflow-hidden h-[35vh] md:h-[50vh] lg:h-[60vh]"
               onMouseEnter={() => setIsPaused(true)} // Tạm dừng khi hover
               onMouseLeave={() => setIsPaused(false)}
             >
@@ -97,7 +197,7 @@ const EatonPark = () => {
                   }}
                 >
                   {/* Overlay thông tin */}
-                  <div className="absolute bottom-4 left-4 bg-black bg-opacity-60 text-white p-4 rounded-lg">
+                  <div className="absolute bottom-4 left-4 bg-black bg-opacity-60 text-white p-4 rounded">
                     <h2 className="text-lg md:text-2xl lg:text-4xl font-bold">
                       {slide.content}
                     </h2>
@@ -230,7 +330,6 @@ const EatonPark = () => {
                   Tiêu chuẩn bàn giao
                 </p>
               </div>
-
               {/* Cột bên phải */}
               <div className="flex-1">
                 <p className="text-sm text-gray-700 mb-3">Eaton Park</p>
@@ -249,46 +348,38 @@ const EatonPark = () => {
           <div></div>
         </div>
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-[2fr_1fr] lg:grid-cols-[2fr_1fr]">
-          <div className="bg-gray-50 rounded mt-6 p-6">
-            <h2 className="text-xl font-bold text-gray-800 mb-3">Vị trí</h2>
+          <div className="rounded mt-6 py-6">
+            <h2 className="text-xl font-bold text-gray-800 mb-2">
+              Vị trí mặt bằng
+            </h2>
             <div
-              className="w-full mx-auto rounded relative overflow-hidden h-[35vh] md:h-[50vh] lg:h-[55vh]"
+              className="w-full mx-auto rounded relative overflow-hidden h-[50vh] md:h-[55vh] lg:h-[70vh]"
               onMouseEnter={() => setIsPaused(true)} // Tạm dừng khi hover
               onMouseLeave={() => setIsPaused(false)}
             >
-              {slides.map((slide, index) => (
+              {areas.map((area, index) => (
                 <div
-                  key={slide.id}
+                  key={area.id}
                   className={`absolute inset-0 transition-transform duration-1000 ease-in-out ${
                     index === currentIndex
                       ? "translate-x-0"
                       : "translate-x-full"
                   }`}
                   style={{
-                    backgroundImage: `url(${slide.image})`,
-                    backgroundSize: "cover", // Ensures the image covers the entire area
+                    backgroundImage: `url(${area.image})`,
+                    backgroundSize: "contain", // Ensures the image is fully visible and scaled
                     backgroundPosition: "center", // Centers the image
                     backgroundRepeat: "no-repeat", // Prevents tiling
                     width: "100%", // Set width to 100% of the parent container
                     height: "100%", // Adjust height as needed
                   }}
-                >
-                  {/* Overlay thông tin */}
-                  <div className="absolute bottom-4 left-4 bg-black bg-opacity-60 text-white p-4 rounded-lg">
-                    <h2 className="text-lg md:text-2xl lg:text-4xl font-bold">
-                      {slide.content}
-                    </h2>
-                    <p className="text-sm md:text-base">
-                      {slide.description || "Mô tả chi tiết cho ảnh này."}
-                    </p>
-                  </div>
-                </div>
+                ></div>
               ))}
               <button
                 className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-3 rounded-full"
                 onClick={() =>
                   setCurrentIndex((prevIndex) =>
-                    prevIndex === 0 ? slides.length - 1 : prevIndex - 1
+                    prevIndex === 0 ? areas.length - 1 : prevIndex - 1
                   )
                 }
               >
@@ -298,12 +389,166 @@ const EatonPark = () => {
                 className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-3 rounded-full"
                 onClick={() =>
                   setCurrentIndex((prevIndex) =>
-                    prevIndex === slides.length - 1 ? 0 : prevIndex + 1
+                    prevIndex === areas.length - 1 ? 0 : prevIndex + 1
                   )
                 }
               >
                 ▶
               </button>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-[2fr_1fr] lg:grid-cols-[2fr_1fr]">
+          <div className="bg-gray-50 rounded mt-6 p-6">
+            <h2 className="text-xl font-bold text-gray-800 mb-3">Tiện ích</h2>
+            <hr className="mb-6" />
+            <div className="flex flex-col sm:flex-row sm:space-x-4 sm:space-y-0">
+              <div className="flex-1">
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">
+                    Hồ bơi tiêu chuẩn hạng sang
+                  </p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">
+                    Khu chiêm nghưỡng phong cảnh
+                  </p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">Khu vui chơi trẻ em</p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">Sân cỏ đi bộ</p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">Khu BBQ</p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">
+                    Khu đọc sách ngoài trời
+                  </p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">
+                    Khu chiếu phim ngoài trời
+                  </p>
+                </div>
+              </div>
+              <div className="flex-1">
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">
+                    Khu tập thể dục ngoài trời
+                  </p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">Hồ bơi Trẻ em</p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">Hồ Jacuzzi</p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">Sảnh thiền và yoga</p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">Phòng Gym</p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">
+                    Đường chạy bộ trên cao
+                  </p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">Thư viện đọc sách</p>
+                </div>
+              </div>
+              <div className="flex-1">
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">
+                    Khu bóng đá/Khu bóng rổ
+                  </p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">Suối nước phong thủy</p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">Khu thảo mộc</p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">Khu thực tế ảo</p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">Thác nước</p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">Vườn treo</p>
+                </div>
+                <div className="flex items-center mb-3">
+                  <CheckIcon className="w-4 h-4 text-gray-500" />
+                  <p className="text-sm text-gray-700">Khu sinh hoạt cư dân</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-[2fr_1fr] lg:grid-cols-[2fr_1fr]">
+          <div className="rounded mt-12">
+            <h2 className="text-xl font-bold text-gray-800 mb-3">
+              Thư viện ảnh
+            </h2>
+            <div className="flex flex-wrap">
+              {/* Các ảnh */}
+              {slides.map((slide) => (
+                <div
+                  key={slide.id}
+                  className="w-full sm:w-1/2 lg:w-1/3 p-1 transition-transform transform hover:scale-105 cursor-pointer"
+                  onClick={() => openModal(slide.image)} // Quy định số cột ở từng kích thước màn hình
+                >
+                  <div
+                    className="w-full rounded h-64 sm:h-48 bg-cover bg-center"
+                    style={{
+                      backgroundImage: `url(${slide.image})`,
+                    }}
+                  ></div>
+                </div>
+              ))}
+              {/* Modal */}
+              {isModalOpen && (
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+                  <div className="relative">
+                    <button
+                      className="absolute top-4 right-4 text-white text-3xl"
+                      onClick={closeModal}
+                    >
+                      ✖
+                    </button>
+                    <img
+                      src={modalImage}
+                      alt="Larger view"
+                      className="max-w-full max-h-full object-contain rounded"
+                    />
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
